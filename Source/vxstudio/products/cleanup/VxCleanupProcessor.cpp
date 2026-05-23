65fa52990023c4d17a57df1ba38bf2ba2237d874
Andrzej Marczewski <daverage@gmail.com>
2026-05-23 13:23:25 +0100

    Revert "Skip persistent clarity stage until cleanup >= 15%"
    This reverts commit 5690be6ac89bef0fd4fff0a2c7012705cf6f3c62.

--- Source/vxstudio/products/cleanup/VxCleanupProcessor.cpp
+++ Source/vxstudio/products/cleanup/VxCleanupProcessor.cpp
@@ -350,7 +350,7 @@ void VXCleanupAudioProcessor::processProduct(juce::AudioBuffer<float>& buffer, j
         1.00f + 0.84f * cleanupRamp
       + 1.72f * cleanupRamp * densityPressure * voicedMaterialGuard);
 
-    if (cleanup > 0.15f) {
+    if (cleanup > 1.0e-4f) {
         vxsuite::clarity::Params persistentParams {};
         persistentParams.clean = juce::jlimit(0.0f, 1.0f,
             (cleanupRamp * (0.46f + 0.32f * densityPressure)
